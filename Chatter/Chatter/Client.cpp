913f9fc78d9834f84b97443178a6c6085f5945f1
adam <adamengel1@gmail.com>
2017-11-22 11:24:32 +0200

    add packages.config and the functions frames

--- Chatter/Chatter/Client.cpp
+++ Chatter/Chatter/Client.cpp
@@ -1,5 +1,5 @@
 #include "Client.h"
-
+#include "cpprest\http_client.h"
 
 
 Client::Client()
@@ -9,4 +9,39 @@ Client::Client()
 
 Client::~Client()
 {
+}
+
+bool Client::Login(string username, string password)
+{
+	return false;
+}
+
+string Client::GetActiveUsers()
+{
+	return string();
+}
+
+string Client::GetMessage(string fromUser)
+{
+	return string();
+}
+
+bool Client::Send(string fromUser, string toUser, string message)
+{
+	return false;
+}
+
+bool Client::SendAll()
+{
+	return false;
+}
+
+bool Client::Ban(string username)
+{
+	return false;
+}
+
+bool Client::Unban(string username)
+{
+	return false;
 }
