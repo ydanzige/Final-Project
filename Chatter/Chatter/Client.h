9fbc5166b78cef595bfb85da037258a6eb60b576
unknown <ahuri@g.jct.ac.il>
2017-11-22 10:58:44 +0200

    Adding URLs

--- Chatter/Chatter/Client.h
+++ Chatter/Chatter/Client.h
@@ -15,13 +15,12 @@ public:
 	Client();
 	~Client();
 
-	bool Login(string username, string password);
-	string GetActiveUsers();
-	string GetMessage(string fromUser);
-	bool Send(string fromUser, string toUser, string message);
-	bool SendAll();
-	bool Ban(string username);
-	bool Unban(string username);
-
+	bool Login(string username, string password);	// login?username=<username>&password=<password>
+	string GetActiveUsers();	// getActiveUser
+	string GetMessage(string fromUser);	// getMessages?username=<username>
+	bool Send(string fromUser, string toUser, string message);	// send?username=<username>&massege=<massege>
+	bool SendAll();	// sendAll?username=<username>&massege=<massege>
+	bool Ban(string username);		// banUser
+	bool Unban(string username);	// unbanUser
 };
 
