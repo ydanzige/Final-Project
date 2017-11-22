8498b51780a4613c6d308474f6ddcb55cf6e6bfb
unknown <ahuri@g.jct.ac.il>
2017-11-22 10:50:39 +0200

    Fixing all supported functions prototypes

--- Chatter/Chatter/Client.h
+++ Chatter/Chatter/Client.h
@@ -6,16 +6,19 @@ using namespace std;
 class Client
 {
 private:
+	const string SERVER = "server";
+	const int PORT = 1234;
+
 	string m_username;
-	string m_serverName;
-	int m_port;
+	string m_password;
 public:
 	Client();
 	~Client();
 
-	bool Login(string serverName, int port, string username);
-	string ActiveUsers();
-	bool Send(string username);
+	bool Login(string username, string password);
+	string GetActiveUsers();
+	string GetMessage(string fromUser);
+	bool Send(string fromUser, string toUser, string message);
 	bool SendAll();
 	bool Ban(string username);
 	bool Unban(string username);
