0699e33acd6400acb016fe22912a1329690b3d6e
ahuri <ahuri@jct.ac.il>
2017-11-22 16:10:05 +0200

    Fixes

--- Chatter/Chatter/Client.h
+++ Chatter/Chatter/Client.h
@@ -20,12 +20,13 @@ public:
 	~Client();
 
 	bool Login(string username, string password);
-	string GetActiveUsers();	
-	string GetMessage(string fromUser);	
-	void Send(string fromUser, string toUser, string message);	
-	bool SendAll();	
-	bool Ban(string username);		
-	bool Unban(string username);	
+	string GetActiveUsers();
+	string GetMessage(string fromUser);
+	bool Send(string fromUser, string toUser, string message);
+	bool SendAll();
+	bool Ban(string username);
+	bool Unban(string username);
+
 	pplx::task<value> sendPostRequest(value jsonObj);
 };
 
